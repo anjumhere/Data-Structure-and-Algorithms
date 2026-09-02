c0f7ff0909a6657d01d3408bec09807b5827644d
Adnan Anjum <anjumhere.dev@gmail.com>
2026-09-02 23:48:19 +0500

    add solution to pattern 1

--- patterns/pat1.cpp
+++ patterns/pat1.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 
 /*
-* pattern 1
-              *
-            * * *
-          * * * * *
-        * * * * * * *
-      * * * * * * * * *
+         pattern 1
+
+   0           *               {4, 1, 4}
+   1         * * *             {3, 3, 3}
+   2       * * * * *           {2, 5, 2}
+   3     * * * * * * *         {1, 7, 1}
+   4   * * * * * * * * *       {0, 10,0}
 
 
 */
 
-int main() { return 0; }
+/*
+
+        Solution
+    we have 5 rows , now we know the outer loop;
+
+   0           *               {4, 1, 4}
+   1         * * *             {3, 3, 3}
+   2       * * * * *           {2, 5, 2}
+   3     * * * * * * *         {1, 7, 1}
+   4   * * * * * * * * *       {0, 10,0}
+
+if we see the pattern , there is nothing but.
+
+space + stars + space
+
+
+*/
+int main() {
+
+  for (int i = 0; i < 5; i++) {
+  }
+  return 0;
+}
